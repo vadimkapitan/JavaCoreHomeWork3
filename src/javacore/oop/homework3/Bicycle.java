0d8e6e96ca7991ff77fd0f8196999541699527cb
vadimKapitan <xxxcap13@gmail.com>
2023-09-03 21:30:02 +0300

    Готовое ДЗ

--- src/javacore/oop/homework3/Bicycle.java
+++ src/javacore/oop/homework3/Bicycle.java
@@ -4,8 +4,4 @@ public class Bicycle extends Transport{
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
-    @Override
-    public void service() {
-        super.service();
-    }
 }
--- src/javacore/oop/homework3/Truck.java
+++ src/javacore/oop/homework3/Truck.java
@@ -1,11 +1,8 @@
 package javacore.oop.homework3;
 
-public class Truck extends Transport {
+public class Truck extends Car {
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
-    }
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
     }
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
